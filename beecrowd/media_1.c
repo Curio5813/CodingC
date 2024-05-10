751ea76124179f6aadf5a5127fcbb0552de8ebee
Curio5813 <marcosglasner@gmail.com>
2024-05-10 11:03:35 +0100

    Adding program media_1.c to the repository.

---
+++ beecrowd/media_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main(){
+
+    double nota1, nota2, media;
+
+    scanf("%lf %lf", &nota1, &nota2);
+
+    media = (nota1 * 3.5 + nota2 * 7.5) / 11.0;
+
+    printf("MEDIA = %.5lf\n", media);
+
+
+    return 0;
+}
