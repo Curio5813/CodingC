59c1d73887266ff19034d0ec9f569639d59a10c3
Curio5813 <marcosglasner@gmail.com>
2024-05-14 17:26:34 +0100

    Adding program novo_recorde.c to the repository.

---
+++ beecrowd/novo_recorde.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int n, t, d;
+    double vm, maior = 0.0;
+
+    while(scanf("%d", &n) != EOF){
+        for(int i = 0; i < n; i++){
+            scanf("%d %d", &t, &d);
+            vm = (double)d / (double)t;
+            //printf("%.2lf %.2lf\n", vm, maior);
+            if(i == 0){
+                printf("%d\n", i + 1);
+                maior = vm;
+            }
+            if(i > 0){
+                if(vm > maior){
+                    printf("%d\n", i + 1);
+                    maior = vm;
+                }
+            }
+
+        }
+    }
+
+    return 0;
+}
