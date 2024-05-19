80e39f0daed96b650063f201a41759acccdfb47c
Curio5813 <marcosglasner@gmail.com>
2024-05-19 02:51:21 +0100

    Adding program pares_entre_cinco_numeros.c to the repository.

---
+++ beecrowd/pares_entre_cinco_numeros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int num, cont = 0;
+
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &num);
+        if(num % 2 == 0){
+            cont ++;
+        }
+    }
+    printf("%d valores pares\n", cont);
+
+    return 0;
+}
