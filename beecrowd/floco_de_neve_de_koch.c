e2cc5ded57cc969034e7c6a22f08c1cb8a3e4336
Curio5813 <marcosglasner@gmail.com>
2024-05-27 16:44:32 +0100

    Adding the program floco_de_neve_de_koch.c to the repository.

---
+++ beecrowd/floco_de_neve_de_koch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+
+    double lado, area, area_total;
+
+    while(scanf("%lf", &lado)){
+        area = ((lado * lado) * sqrt(3)) / 4.0;
+        area_total = (8 * area) / 5;
+        printf("%.2lf\n", area_total);
+    }
+
+    return 0;
+}
