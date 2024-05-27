db95b4af63db314ba892bf23b8fcd4f36aad9132
Curio5813 <marcosglasner@gmail.com>
2024-05-27 16:48:13 +0100

    Updating the program floco_de_neve_de_koch.c

--- beecrowd/floco_de_neve_de_koch.c
+++ beecrowd/floco_de_neve_de_koch.c
@@ -6,7 +6,7 @@ int main(){
 
     double lado, area, area_total;
 
-    while(scanf("%lf", &lado)){
+    while(scanf("%lf", &lado) != EOF){
         area = ((lado * lado) * sqrt(3)) / 4.0;
         area_total = (8 * area) / 5;
         printf("%.2lf\n", area_total);
