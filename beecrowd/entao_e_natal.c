bef33ebad6af31443c15f33c5979df37bd5be690
Curio5813 <marcosglasner@gmail.com>
2024-06-10 18:49:21 +0100

    Adding program entao_e_natal.c to the repository.

---
+++ beecrowd/entao_e_natal.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int i;
+    char frase[] = "Entao eh Natal!";
+
+    scanf("%d", &i);
+    printf("Ent");
+    for(int j = 0; j < i; j++){
+        printf("a");
+    }
+    printf("o");
+    printf(" eh N");
+    for(int j = 0; j < i; j++){
+        printf("a");
+    }
+    printf("t");
+    for(int j = 0; j < i; j++){
+        printf("a");
+    }
+    printf("l!\n");
+
+    return 0;
+}
