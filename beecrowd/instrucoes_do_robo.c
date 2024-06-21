bf60573591aff637bb9afc1e5c31da11ed642d5f
Curio5813 <marcosglasner@gmail.com>
2024-06-21 15:49:50 +0100

    Adding program instrucoes_do_robo.c to the repository. The program still under debugging.

---
+++ beecrowd/instrucoes_do_robo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+
+    int t, n;
+
+    scanf("%d", &t);
+    for(int i = 0; i < t; i++) {
+        int posicao = 0;
+        scanf("%d", &n);
+        char direcoes[n][3];
+    }
+
+    return 0;
+}
