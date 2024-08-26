39004148fe81808d1f0cdbf26f71890e47d26242
Curio5813 <marcosglasner@gmail.com>
2024-08-27 00:13:41 +0100

    The program is under debugging.

--- beecrowd/participando_de_uma_maratona.c
+++ beecrowd/participando_de_uma_maratona.c
@@ -4,21 +4,21 @@
 
 int main(){
 
-    long long int r, p, q, maior = 0, tempo, posicao, idx1 = 0, idx2 = 0, sucesso = 0, descarte;
+    int r, p, q, maior = 0, tempo, posicao, idx1 = 0, idx2, sucesso = 0, descarte;
 
-    scanf("%lld", &r);
-    long long int  corredores[r][2];
-    for(long long int i = 0; i < r; i++){
-        for(long long j = 0; j < 2; j++) {
-            scanf("%lld", &corredores[i][j]);
+    scanf("%d", &r);
+    int  corredores[r][2];
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < 2; j++) {
+            scanf("%d", &corredores[i][j]);
             // printf("%lld ", corredores[i][j]);
         }
     }
-    scanf("%lld", &p);
-    long long int fotos[p][3];
-    for(long long int i = 0; i < p; i++){
-        for(long long j = 0; j < 3; j++) {
-            scanf("%lld", &fotos[i][j]);
+    scanf("%d", &p);
+    int fotos[p][3];
+    for(int i = 0; i < p; i++){
+        for(int j = 0; j < 3; j++) {
+            scanf("%d", &fotos[i][j]);
             // printf("%lld ", fotos[i][j]);
             if(j == 2 && fotos[i][j] > maior){
                 maior = fotos[i][j];
@@ -26,12 +26,11 @@ int main(){
         }
         // printf("\n");
     }
-    //printf("%lld\n", maior);
-    long long int instantes[r][2];
-    int flag = 0;
-    for(long long int i = 0; i < r; i++){
+    int instantes[r][2];
+    for(int i = 0; i < r; i++){
         tempo = 0;
         posicao = 0;
+        printf("%d %d Ok1\n", maior, r);
         while(posicao <= maior){
             if(tempo < corredores[i][0]){
                 tempo += 1;
@@ -39,76 +38,62 @@ int main(){
             else if(tempo >= corredores[i][0]){
                 tempo += 1;
                 posicao += corredores[i][1];
-            }
-            for(long long int j = 0; j < r; j++){
-                if(tempo != instantes[j][0] && posicao != instantes[j][1]){
-                    flag = 1;
-                }
-            }
-            if(flag == 1){
                 instantes[idx1][0] = tempo;
                 instantes[idx1][1] = posicao;
-                flag = 0;
+                printf("%d %d\n", instantes[idx1][0], instantes[idx1][1]);
                 idx1 += 1;
             }
         }
     }
-    scanf("%lld", &q);
-    long long int  querys[q][2];
-    for(long long int i = 0; i < q; i++){
-        for(long long j = 0; j < 2; j++) {
-            scanf("%lld", &querys[i][j]);
-            // printf("%lld ", querys[i][j]);
+    printf("%d Ok2\n", maior);
+    scanf("%d", &q);
+    int  querys[q][2];
+    for(int i = 0; i < q; i++){
+        for(int j = 0; j < 2; j++) {
+            scanf("%d", &querys[i][j]);
+            //printf("%lld ", querys[i][j]);
         }
         //printf("\n");
     }
-    long long int instantes2[q][2];
-    flag = 0;
-    for(long long int i = 0; i < idx1; i++){
-        for(long long j = 0; j < 2; j++){
-            instantes2[i][j] = instantes[i][j];
-            // printf("%lld ", instantes2[i][j]);
-        }
-        // printf("\n");
-    }
-    //printf("%lld\n", maior);
-    for(long long int i = 0; i < q; i++) {
+    for(int i = 0; i < q; i++) {
         tempo = 0;
         posicao = 0;
+        idx2 = idx1;
+        //printf("%lld %lld %lld\n", q, idx1, maior);
+        int instantes2[q][2];
+        for(int j = 0; j < idx1; j++){
+            for(int k = 0; k < 2; k++){
+                instantes2[j][k] = instantes[j][k];
+                //printf("%lld ", instantes2[i][j]);
+            }
+            //printf("\n");
+        }
         //printf("%lld\n", maior);
         while (posicao <= maior) {
             if (tempo < querys[i][0]) {
                 tempo += 1;
-            } else if (tempo >= querys[i][0]) {
+            }
+            if (tempo >= querys[i][0]) {
                 tempo += 1;
                 posicao += querys[i][1];
             }
-            for (long long int j = 0; j < q; j++) {
-                if (tempo != instantes2[j][0] && posicao != instantes2[j][1]) {
-                    flag = 1;
-                }
-            }
-            if (flag == 1) {
-                instantes2[idx2][0] = tempo;
-                instantes2[idx2][1] = posicao;
-                flag = 0;
-                idx2 += 1;
-            }
+            instantes2[idx2][0] = tempo;
+            instantes2[idx2][1] = posicao;
+            idx2 += 1;
+            //printf("%lld\n", idx2);
         }
-        //printf("%lld\n", idx2);
-        for(long long int j = 0; j < p; j++){
-            for(long long int k = 0; k < idx2; k++) {
-                printf("%lld ", instantes2[j][k]);
+        for(int j = 0; j < p; j++){
+            for(int k = 0; k < idx2; k++) {
+                //printf("%lld ", instantes2[j][k]);
                 if (instantes2[k][0] == fotos[j][0] && fotos[j][1] <= instantes2[k][1] && instantes2[k][1] <= fotos[j][2]) {
                     sucesso += 1;
                     //printf("%lld\n", sucesso);
                     break;
                 }
             }
-            printf("\n");
         }
         descarte = q - sucesso;
-        //printf("%lld %lld\n", sucesso, descarte);
+        printf("%d %d\n", sucesso, descarte);
         //printf("%lld\n", descarte);
         sucesso = 0;
     }
