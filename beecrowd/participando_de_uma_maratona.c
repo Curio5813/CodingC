f622cbdfcaf6f809b51a524aacad0c2820d2e79c
Curio5813 <marcosglasner@gmail.com>
2024-08-27 02:02:02 +0100

    Debugging program.

--- beecrowd/participando_de_uma_maratona.c
+++ beecrowd/participando_de_uma_maratona.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 
 int main(){
 
-    int r, p, q, maior = 0, tempo, posicao, idx1 = 0, idx2, sucesso = 0, descarte;
+    long long int r, p, q, maior = 0, tempo, posicao, idx1 = 0, idx2, sucesso = 0, descarte;
 
-    scanf("%d", &r);
-    int  corredores[r][2];
-    for(int i = 0; i < r; i++){
-        for(int j = 0; j < 2; j++) {
-            scanf("%d", &corredores[i][j]);
+    scanf("%lld", &r);
+    long long int  corredores[r][2];
+    for(long long int i = 0; i < r; i++){
+        for(long long int j = 0; j < 2; j++) {
+            scanf("%lld", &corredores[i][j]);
             // printf("%lld ", corredores[i][j]);
         }
     }
-    scanf("%d", &p);
-    int fotos[p][3];
-    for(int i = 0; i < p; i++){
-        for(int j = 0; j < 3; j++) {
-            scanf("%d", &fotos[i][j]);
+    scanf("%lld", &p);
+    long long int fotos[p][3];
+    for(long long int i = 0; i < p; i++){
+        for(long long int j = 0; j < 3; j++) {
+            scanf("%lld", &fotos[i][j]);
             // printf("%lld ", fotos[i][j]);
             if(j == 2 && fotos[i][j] > maior){
                 maior = fotos[i][j];
@@ -26,11 +25,10 @@ int main(){
         }
         // printf("\n");
     }
-    int instantes[r][2];
-    for(int i = 0; i < r; i++){
+    long long int instantes[10000000][2];
+    for(long long int i = 0; i < r; i++){
         tempo = 0;
         posicao = 0;
-        printf("%d %d Ok1\n", maior, r);
         while(posicao <= maior){
             if(tempo < corredores[i][0]){
                 tempo += 1;
@@ -38,31 +36,30 @@ int main(){
             else if(tempo >= corredores[i][0]){
                 tempo += 1;
                 posicao += corredores[i][1];
-                instantes[idx1][0] = tempo;
-                instantes[idx1][1] = posicao;
-                printf("%d %d\n", instantes[idx1][0], instantes[idx1][1]);
-                idx1 += 1;
             }
+            instantes[idx1][0] = tempo;
+            instantes[idx1][1] = posicao;
+            idx1 += 1;
         }
+        //printf("%d %d Ok1\n", maior, r);
     }
-    printf("%d Ok2\n", maior);
-    scanf("%d", &q);
-    int  querys[q][2];
-    for(int i = 0; i < q; i++){
-        for(int j = 0; j < 2; j++) {
-            scanf("%d", &querys[i][j]);
+    scanf("%lld", &q);
+    long long int  querys[q][2];
+    for(long long int i = 0; i < q; i++){
+        for(long long int j = 0; j < 2; j++) {
+            scanf("%lld", &querys[i][j]);
             //printf("%lld ", querys[i][j]);
         }
         //printf("\n");
     }
-    for(int i = 0; i < q; i++) {
+    for(long long int i = 0; i < q; i++) {
         tempo = 0;
         posicao = 0;
         idx2 = idx1;
         //printf("%lld %lld %lld\n", q, idx1, maior);
-        int instantes2[q][2];
-        for(int j = 0; j < idx1; j++){
-            for(int k = 0; k < 2; k++){
+        long long int instantes2[10000000][2];
+        for(long long int j = 0; j < idx1; j++){
+            for(long long int k = 0; k < 2; k++){
                 instantes2[j][k] = instantes[j][k];
                 //printf("%lld ", instantes2[i][j]);
             }
@@ -82,8 +79,8 @@ int main(){
             idx2 += 1;
             //printf("%lld\n", idx2);
         }
-        for(int j = 0; j < p; j++){
-            for(int k = 0; k < idx2; k++) {
+        for(long long int j = 0; j < p; j++){
+            for(long long int k = 0; k < idx2; k++) {
                 //printf("%lld ", instantes2[j][k]);
                 if (instantes2[k][0] == fotos[j][0] && fotos[j][1] <= instantes2[k][1] && instantes2[k][1] <= fotos[j][2]) {
                     sucesso += 1;
@@ -93,8 +90,8 @@ int main(){
             }
         }
         descarte = q - sucesso;
-        printf("%d %d\n", sucesso, descarte);
-        //printf("%lld\n", descarte);
+        //printf("%d %d\n", sucesso, descarte);
+        printf("%lld\n", descarte);
         sucesso = 0;
     }
     return 0;
