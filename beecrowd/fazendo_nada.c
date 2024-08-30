99ff967457149188df6d87fc25abbf9b93df43c5
Curio5813 <marcosglasner@gmail.com>
2024-08-30 02:19:59 +0100

    Depurando o programa fazendo_nada.c

--- beecrowd/fazendo_nada.c
+++ beecrowd/fazendo_nada.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-
+#include <gmp.h>
 
 int main(){
 
     long long int n, m;
 
     while(scanf("%lld %lld", &n, &m)){
-        long long int capitulos[n], minutos = 0;
+        long long int capitulos[n];
+        mpz_t minutos;
+        mpz_t temp1;
+        mpz_t temp2;
+        mpz_t serie;
+        mpz_init(minutos);
+        mpz_init(temp1);
+        mpz_init(temp2);
+        mpz_init(serie);
+        char input[1024];
+
         if(n == -1 && m == -1){
             break;
         }
@@ -17,11 +27,15 @@ int main(){
         }
         for(long long int i = 0; i < n; i++){
             for(long long int j = 0; j <= i; j++){
-                minutos += capitulos[j] * m;
+                mpz_set_ui(temp1, capitulos[i]);
+                mpz_mul(serie, temp1, (mpz_srcptr) capitulos[j]);
+
             }
         }
-        printf("%lld\n", minutos);
-        // teste
+        gmp_printf("%Zd\n", serie);
+        mpz_clear(serie);
+        mpz_clear(temp1);
+
     }
 
     return 0;
