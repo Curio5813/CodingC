eb56491daafdb70e5dea65a4264561cc885eaa5f
Curio5813 <marcosglasner@gmail.com>
2024-09-01 17:17:11 +0100

    Debugando programa fazendo_nada.c

--- beecrowd/fazendo_nada.c
+++ beecrowd/fazendo_nada.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
-#include <gmp.h>
 #include <stdlib.h>
+#include <gmp.h>
+
 
 int main(){
 
-    long long int n, m;
+    int n, m;
 
-    while(scanf("%lld %lld", &n, &m)){
-        long long int *capitulos;
-        long long int minutos = 0;
-
-        capitulos = (long long int*) malloc (n * sizeof(long long int));
-
-        if(n == -1 && m == -1){
+    while(scanf("%d %d", &n, &m)) {
+        if (n == -1 && m == -1) {
             break;
         }
-        else{
-            for(long long int i = 0; i < n; i++) {
-                scanf("%lld", &capitulos[i]);
+        mpz_t num1, num2, num3, minutos, produto;
+        mpz_init_set_ui(num1, 1);
+        mpz_init_set_ui(num2, 0);
+        mpz_init_set_ui(num3, m);
+        mpz_t *capitulos_gmp = (mpz_t *)malloc(n * sizeof(mpz_t));
+        if (n == -1 && m == -1) {
+            break;
+        } else {
+            for (int i = 0; i < n; i++) {
+                mpz_init(capitulos_gmp[i]);
             }
-        }
-        for(long long int i = 0; i < n; i++){
-            for(long long int j = 0; j <= i; j++){
-                minutos += capitulos[j] * m;
+            for (int i = 0; i < n; i++) {
+                gmp_scanf("%Zd", capitulos_gmp[i]);
             }
+            mpz_set(produto, num1);
+            mpz_set(minutos, num2);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    mpz_mul(produto, capitulos_gmp[j], num3);
+                    mpz_add(minutos, minutos, produto);
+                }
+            }
+            gmp_printf("%Zd\n", minutos);
+            mpz_clear(minutos);
+            mpz_clear(produto);
+            mpz_clear(capitulos_gmp[n]);
         }
-        printf("%lld\n", minutos);
-        free(capitulos);
-        capitulos = NULL;
     }
 
     return 0;
 }
+
