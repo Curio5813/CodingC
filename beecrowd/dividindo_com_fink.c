afcfdcac989c3f61ab48e097a1597228dad77683
Curio5813 <marcosglasner@gmail.com>
2024-09-06 13:35:09 +0100

    Adding program dividindo_com_fink.c to the repository. The program dosen't passed on TLE. Itstill under debugging.

---
+++ beecrowd/dividindo_com_fink.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+int main(){
+
+    long int n;
+
+    while(scanf("%ld", &n)){
+        long int raposa = 0, picapau = 0, comida = n, flag = 0 , div_raposa, div_picapau;
+        if(n == 0){
+            break;
+        }
+        else{
+            for(int i = 1; i <= n; i++) {
+                picapau += 1;
+                div_picapau = picapau;
+                for(int j = 1; j <= i; j++){
+                    if((div_raposa + div_picapau) > comida){
+                        flag = 1;
+                    }
+                    else{
+                        raposa += j;
+                    }
+                }
+                if(flag != 1){
+                    div_raposa = raposa;
+                    raposa = 0;
+                }
+            }
+        }
+        while(raposa < div_raposa){
+            picapau -= 1;
+            raposa += 1;
+        }
+        printf("%ld %ld\n", raposa, picapau);
+    }
+
+    return 0;
+}
