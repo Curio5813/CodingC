c167bffd775a0a1dcf607df645f026bdf1126f7c
Curio5813 <marcosglasner@gmail.com>
2026-06-28 23:51:10 +0100

    Executando alguns arquivos em C.

--- beecrowd/bafo.c
+++ beecrowd/bafo.c
@@ -10,12 +10,10 @@ int main(){
         if(r == 0){
             break;
         }
-        else{
-            for(int i = 0; i < r; i++){
-                scanf("%d %d", &a, &b);
-                aldo += a;
-                beto += b;
-            }
+        for(int i = 0; i < r; i++){
+            scanf("%d %d", &a, &b);
+            aldo += a;
+            beto += b;
         }
         printf("Teste %d\n", cont);
         if(aldo > beto){
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/marcos/Computacao/CodingC
+# Install script for directory: /home/marcos/Computacao/CodingInC
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/marcos/Computacao/CodingC/cmake-build-debug/install_local_manifest.txt"
+  file(WRITE "/home/marcos/Computacao/CodingInC/cmake-build-debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/marcos/Computacao/CodingC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/marcos/Computacao/CodingInC/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
